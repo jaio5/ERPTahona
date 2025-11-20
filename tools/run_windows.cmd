7be22107acea29e5795b41c23e124bda3c43b4ac
Javi <Javierbarcelo2105@hotmail.com>
2025-11-20 21:16:56 +0100

    borramos las cosas que ya no nos sirven

--- tools/run_windows.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Script para arrancar ERP en Windows en ventana separada y guardar logs en run.log
-REM Ejecuta desde el directorio del repo: tools\run_windows.cmd
-cd /d "%~dp0\.."
-echo Iniciando ERP desde: %CD%
-start "ERP" cmd /c "java -jar target\ERP-0.0.1-SNAPSHOT.jar > "%CD%\run.log" 2>&1"
-echo Aplicacion iniciada en ventana separada. Revisa %CD%\run.log para logs.
-pause
-
